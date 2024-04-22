a8ce8d3789368d9c996161c1bdbf595c88b9b869
jgarcia11299 <64043191+jgarcia11299@users.noreply.github.com>
2024-04-22 12:15:45 -0700

    Source Code changes

---
+++ Q5 Source Code & Video/Source Code/winter.lua
@@ -0,0 +1,19 @@
+--located in data/spells/scripts/attack
+
+local combat = Combat()
+combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_ICEDAMAGE)
+combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
+combat:setArea(createCombatArea(AREA_FRIGO3X3))
+
+
+function onGetFormulaValues(player, level, magicLevel)
+	local min = (level / 5) + (magicLevel * 5.5) + 25
+	local max = (level / 5) + (magicLevel * 11) + 50
+	return -min, -max
+end
+
+combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
+
+function onCastSpell(creature, variant)
+	return combat:execute(creature, variant)
+end
